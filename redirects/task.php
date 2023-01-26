22f2867eb5f24c9f7b75a05e52d0989f2994348d
MIA ISABELLA ISABELLA <113313205+isabee07@users.noreply.github.com>
2023-01-26 19:12:54 +0000

    idk

--- redirects/task.php
+++
@@ -1,7 +0,0 @@
-
-<?php $_SERVER["DOCUMENT_ROOT"] . "/functions.php"; 
-
-$jsonFormData = file_get_contents($_SERVER[DOCUMENT_ROOT] . '/data/tasks.json'); 
-$pastFormData = $json_decode($jsonFormData, TRUE); 
-
-?>
