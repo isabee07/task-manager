e9ea80310bad324d85d97aaa69724cb5251a1421
MIA ISABELLA ISABELLA <113313205+isabee07@users.noreply.github.com>
2023-01-30 18:31:24 +0000

    pushing this thing ig

--- redirects/task.php
+++ redirects/task.php
@@ -4,4 +4,10 @@
 $jsonFormData = file_get_contents($_SERVER[DOCUMENT_ROOT] . '/data/tasks.json'); 
 $pastFormData = $json_decode($jsonFormData, TRUE); 
 
+ $_SESSION["form"]["fName"] = $_POST['fName'];
+ $_SESSION["form"]["lName"] = $_POST['lName'];
+ $_SESSION["form"]["email"] = $_POST['email'];
+ $_SESSION["form"]["age"] = $_POST['age'];
+ $_SESSION["form"]["gender"] = $_POST['gender'];
+ $_SESSION["form"]["relationship"] = $_POST['relationship'];
 ?>
