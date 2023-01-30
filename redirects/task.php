1ad5db2126e3712713ab9feae6ecac0f689e1e48
MIA ISABELLA ISABELLA <113313205+isabee07@users.noreply.github.com>
2023-01-30 18:54:38 +0000

    starting redirect page; waiting for json root to finish

--- redirects/task.php
+++ redirects/task.php
@@ -1,9 +1,30 @@
 <?php $_SERVER["DOCUMENT_ROOT"] . "/functions.php"; 
+
+
 $largest_uid = 0; 
 foreach($tasksData as $item) {
   if ($item['uid'] > $largest_uid) {
     $largest_uid = $item['uid']; 
-    $new
+    $newUID = $largest_uid+1;
   }
 }
+
+$newFormData = array(
+  "uid"=>$newUID,
+  "userUID"=>$_POST['user'],
+  "dateCreate"=>date(Y/m/d),
+  "dateComplete"=>NULL,
+  "dateDeadline"
+  "title"=>$_POST['title'],
+  "description"=>$_POST['description'],
+  "reward"=>$_POST['reward'],
+  "typeTask"
+  
+); 
+
+array_push(); 
+$jsonData = json_encode(); 
+file_put_contents(); 
+
+
 ?>
