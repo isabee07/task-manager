2faa2bf115ae6d5bcbc01c102d5e8a70a6b4f371
MIA ISABELLA ISABELLA <113313205+isabee07@users.noreply.github.com>
2023-02-08 18:36:35 +0000

    pushing for commits

--- redirects/task.php
+++ redirects/task.php
@@ -1,37 +1,7 @@
-<?php include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
-if(isset($_POST['BTN_create']) > 0) { 
-  
-  $date = date_create();
-$largest_uid = 0; 
-foreach($tasksData as $item) {
-  if ($item['uid'] > $largest_uid) {
-    $largest_uid = $item['uid']; 
-    $newUID = $largest_uid+1;
-  }
-}
+<?php $_SERVER["DOCUMENT_ROOT"] . "/functions.php"; 
 
-$newFormData = array(
-  "uid"=>$newUID,
-  "user"=>$_POST['inputUser'],
-  "dateCreate"=>date(Y/m/d),
-  "dateComplete"=>NULL,
-  "dateDeadline"=>$_POST['inputDuedate'],
-  "title"=>$_POST['inputTitle'],
-  "description"=>$_POST['description'],
-  "reward"=>$_POST['inputreward'],
-  "typeTask"=>$_POST['inputtype'], 
-  "address"=>$_POST['inputAddress']  
-); 
+$jsonFormData = file_get_contents($_SERVER[DOCUMENT_ROOT] . '/data/tasks.json'); 
+$pastFormData = $json_decode($jsonFormData, TRUE); 
 
-$jsonFormData = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/data/.json');
-$pastFormData = json_decode($jsonFormData, TRUE);
-array_push($pastFormData, $newFormData);
-$jsonData = json_encode($pastFormData);
-
-file_put_contents($_SERVER['DOCUMENT_ROOT'].'/data/tasks.json', $jsonData);
-  
-header('Location: /index.php'); 
-  
-}
 ?>
