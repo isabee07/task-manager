9e0a25c50fd81ff39dd001106894d8e99b66e3fb
JAYDEN LANG <119446654+JaydenL06@users.noreply.github.com>
2023-02-08 18:50:27 +0000

    fixed my code and added the due date correctly this time.

--- redirects/task.php
+++ redirects/task.php
@@ -1,7 +1,43 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
-<?php $_SERVER["DOCUMENT_ROOT"] . "/functions.php"; 
+if(isset($_POST['BTN_create']) > 0) { 
+  
+  $date = date_create()
+    $deadlineReformatted = 
+$largest_uid = 0; 
+foreach($tasksData as $item) {
+  if ($item['uid'] > $largest_uid) {
+    $largest_uid = $item['uid']; 
+    $newUID = $largest_uid+1;
+  }
+}
 
-$jsonFormData = file_get_contents($_SERVER[DOCUMENT_ROOT] . '/data/tasks.json'); 
-$pastFormData = $json_decode($jsonFormData, TRUE); 
+$newFormData = array(
+  "uid"=>$newUID,
+  "user"=>$_POST['inputUser'],
+  "dateCreate"=>date(Y/m/d),
+  "dateComplete"=>NULL,
+  "dateDeadline"=>$_POST['inputDuedate']
+  "title"=>$_POST['inputTitle'],
+  "description"=>$_POST['description'],
+  "reward"=>$_POST['inputreward'],
+  "typeTask"=>$_POST['inputtype'], 
+  "address"=>$_POST['inputAddress']  
+); 
 
+
+
+
+array_push($pastFormData, $newFormData); 
+$jsonData = json_encode($newFormData, JSON_PRETTY_PRINT); 
+file_put_contents($pastFormData, $jsonData); 
+
+
+// echo '<pre>'; 
+//   var_dump($newFormData);
+// echo '</pre>'; 
+
+
+header('Location: /index.php'); 
+  
 ?>
