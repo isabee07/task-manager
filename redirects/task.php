855423d978d98dfe949a5e761670165c395889c3
JAYDEN LANG <119446654+JaydenL06@users.noreply.github.com>
2023-02-15 07:44:36 +0000

    I dont know what i did but its not all broken ish.

--- redirects/task.php
+++ redirects/task.php
@@ -3,20 +3,31 @@
 if(isset($_POST['BTN_create']) > 0) { 
 
 
+
+// $largest_uid = 0; 
+// foreach($tasksData as $item) {
+//   if ($item['uid'] > $largest_uid) {
+//     $largest_uid = $item['uid']; 
+//     $newUID = $largest_uid+1;
+//   }
+// }
+  
+
 $newFormData = array(
-  "uid"=>null,
+  "uid"=>$newUID,
   "user"=>$_POST['inputUser'],
   "dateCreate"=>null,
   "dateStart"=> $_POST['inputStartdate'],
   "title"=>$_POST['inputTitle'],
   "description"=>$_POST['description'],
-  "typeTask"=>$_POST['inputtype']
+  "typeTask"=>$_POST['inputtype'],
+  "reward"=>$_POST['inputReward']
 ); 
   
 $jsonTasks = file_get_contents($taskDataFile);
 $tasksData = json_decode($jsonTasks, TRUE);
 array_push($newFormData, $tasksData);
-$jsonData = json_encode($newFormData);
+$jsonData = json_encode($newFormData, JSON_PRETTY_PRINT);
 file_put_contents($_SERVER['DOCUMENT_ROOT'].'/data/tasks.json', $jsonData);
 
 header("Location: /index.php");
