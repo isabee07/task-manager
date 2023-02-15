7f7393f52d8c28ca070e90805fe24dfb98cbc635
JAYDEN LANG <119446654+JaydenL06@users.noreply.github.com>
2023-02-15 19:19:48 +0000

    Added a bunch of stuff like search stuff.

--- redirects/search.php
+++ redirects/search.php
@@ -1,8 +1,26 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
 if(isset($_POST['BTN_create']) > 0) { 
-echo "the button was pressed. <br>";
+
+
+
+  $i=0;
+foreach($tasksData as $task){
+if($task['categories'] == $_POST['filterType'])  {
+  $_SESSION[$i] = $task;
+  $i++;
+  
+  }  //END OF IF
+
+} //END OF FOREACH
+
+
+
+  
+echo '<pre>';
+  var_dump($_SESSION); 
+echo '</pre>';
 } else {
-echo "The button was not pressed. <br>";
+// ummmm error? dont work here
   }
 ?>
