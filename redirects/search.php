2888676dd6d912c99d3ae4ef0273707df85f7e26
XAVIER OGLE <119446889+XavierOgle@users.noreply.github.com>
2023-02-23 19:20:49 +0000

    ChUpdated the variables

--- redirects/search.php
+++ redirects/search.php
@@ -1,46 +1,77 @@
-<?php include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
-
-if(isset($_POST['BTN_create']) > 0) { 
-
-
- if($_POST['filterType'] == 'none' && $_POST['filterUser'] == 'none'){ // if both used
-    header('Location:/tasks/index.php');
-} 
- 
- elseif ($_POST['filterType'] != 'none' && $_POST['filterUser'] == 'none') {  // if cat used
-  $i=0;
-  $_SESSION['searchResults'] = [];
-  foreach($mergedData as $key => $task){
-    if($task['categories'] == $_POST['filterType'])  {
-     $_SESSION['searchResults'][$i] = $task;
-     $i++;   
-    }  
-  } 
-}
-    elseif ($_POST['filterUser'] != 'none' && $_POST['filterType'] == 'none')  { // if user used
-    $i=0;
-  $_SESSION['searchResults'] = [];
-  foreach($mergedData as $key => $task){
-    if($task['userUID'] == $_POST['filterUser'])  {
-     $_SESSION['searchResults'][$i] = $task;
-     $i++; 
-    } 
-  }                                                                             
-    }
-
+<?php /*1st Line on every webpage.*/ include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
   
-    else {
-       $i=0;
-  $_SESSION['searchResults'] = [];
-      foreach($mergedData as $key => $task){
-      if($task['userUID'] == $_POST['filterUser'] && $task['categories'] == $_POST['filterType']) {
-        // if both match
-          $_SESSION['searchResults'][$i] = $task;
-             $i++;    
-      }   // end of if
+  //Clears the session 
+  $_SESSION = array();
   
-    } // end of foreach
- } // end of else
- 
-} // end of isset 
+  //is button pressed?
+  if(isset($_POST['BTN_search'])) {
+    
+    if($_POST['filterType'] == 'none' && $_POST['filterUsers'] == 'none' ) {
+
+      // process if NO filter is selected.
+      header('Location: /tasks/index.php');
+      exit(); //ends the php script.
+      
+    } elseif ($_POST['filterType'] != 'none'&& $_POST['filterUsers'] == 'none' ) {
+       
+        // process if only Category is selected from filter
+        $i = 0;
+        $_SESSION['searchResults'] = [];
+        //loop throught medgedData
+        foreach($mergedTaskUserData as $key => $task) {
+
+            //check to see if search value matched data value
+            if($_POST['filterType'] == $task['categories']) {
+              
+              //search matches data. Build a session. 
+              $_SESSION['searchResults'][$i] = $task; 
+              $i++;
+
+            } //if() inside foreach
+        }//foreach
+      
+    } elseif ($_POST['filterUsers'] != 'none' && $_POST['filterType'] == 'none' ) {
+        
+      // process if only USER is selected from filter
+        $i = 0;
+        $_SESSION['searchResults'] = [];
+        //loop throught medgedData
+        foreach($mergedTaskUserData as $key => $task) {
+
+            //check to see if search value matched data value
+            if($_POST['filterUsers'] == $task['userUID']) {
+              
+              //search matches data. Build a session. 
+              $_SESSION['searchResults'][$i] = $task; 
+              $i++;
+
+            } //if() inside foreach
+          
+        }//foreach
+
+    } else {
+
+      // process if only BOTH user and category are selected from filter
+        $i = 0;
+        $_SESSION['searchResults'] = [];
+        //loop throught medgedData
+        foreach($mergedTaskUserData as $key => $task) {
+            if($_POST['filterUsers'] == $task['userUID'] && $_POST['filterType'] == $task['categories']) { // START:: if both are used
+              //search matches data. Build a session. 
+              $_SESSION['searchResults'][$i] = $task; 
+              $i++;
+                                                                                                            
+            }// END::if both are used
+        }// END::foreach
+    }// END::else
+
+    header('Location: /tasks/index.php?search=yes');
+    exit(); //ends the php script.
+
+  }//if() button pressed
+
+// echo '<pre>';
+// var_dump($mergedTaskUserData);
+// echo '</pre>';
+
 ?>
