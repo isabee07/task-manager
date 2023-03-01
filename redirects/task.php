e689ebc4bb223b61443ba28b22d318385de5db21
MIA ISABELLA ISABELLA <113313205+isabee07@users.noreply.github.com>
2023-03-01 18:24:10 +0000

    added user uid

--- redirects/task.php
+++ redirects/task.php
@@ -3,16 +3,24 @@
 if(isset($_POST['BTN_create']) > 0) { 
 
 $largest_uid = 0;
+$largest_user = 0; 
 foreach ($tasksData as $item) {
     if ($item['uid'] > $largest_uid) {
         $largest_uid = $item['uid'];
         $newUID = $largest_uid+1;
     }
 }
-  
+
+foreach ($usersData as $user) {
+    if ($user['userUID'] > $largest_user) {
+        $largest_user = $user['userUID'];
+        $newUserUID = $largest_user+1;
+    }
+}
 
 $newFormData = array(
   "uid"=>$newUID,
+  "userUID"=>$newUserUID,
   "user"=>$_POST['inputUser'],
   "dateCreate"=>$_POST['inputStartdate'],
   "dateDeadline"=>$_POST['dateDeadline'],
