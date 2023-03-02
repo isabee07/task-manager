afa4d1728d36e2973f0a0e5a83dba6e48e579d24
MIA ISABELLA ISABELLA <113313205+isabee07@users.noreply.github.com>
2023-03-02 18:46:14 +0000

    doing shit

--- index.php
+++ index.php
@@ -162,6 +162,39 @@ echo $cssFiles;
                         </div>
 <!--                       end second card -->
                     </div>
+                   <div class="col-lg-6 col-xxl-4 mb-5">
+<!--                       tenth card -->
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+                                <h2 class="fs-4 fw-bold">Count Tasks</h2>
+                                <p class="mb-0">Random shit</p>
+                            </div>
+                        </div>
+<!--                       end tenth card -->
+                    </div>
+                   <div class="col-lg-6 col-xxl-4 mb-5">
+<!--                       eleventh card -->
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+                                <h2 class="fs-4 fw-bold">Count My sanity</h2>
+                                <p class="mb-0">I'm really tired</p>
+                            </div>
+                        </div>
+<!--                       end eleventh card -->
+                    </div>
+                   <div class="col-lg-6 col-xxl-4 mb-5">
+<!--                       twelfth card -->
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+                                <h2 class="fs-4 fw-bold">Count what's left of my dignity</h2>
+                                <p class="mb-0">God help us all</p>
+                            </div>
+                        </div>
+<!--                       end twelfth card -->
+                    </div>
                   <div class="col-lg-6 col-xxl-4 mb-5">
 <!--                       start seventh card -->
                         <div class="card bg-light border-0 h-100">
--- test.php
+++ test.php
@@ -1,13 +1,7 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/functions.php'); 
-$result = array();
-foreach ($tasksData as $task){
-  foreach ($usersData as $user){
-     if($task["userUID"] == $user["uid"]) {
-      unset($user["uid"]);
-      $result[] = array_merge($task,$user);
-   } 
-  }
-}
-  echo '<pre>';
-  var_dump($result); 
-  echo '</pre>'; ?>
+echo "something";
+
+echo '<pre>'; 
+var_dump($tasksData);
+echo '</pre>'; 
+?>
