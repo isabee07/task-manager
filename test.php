cd64508f557176bc1e856315275f02cb1988bb4b
MIA ISABELLA ISABELLA <113313205+isabee07@users.noreply.github.com>
2023-03-02 19:20:25 +0000

    testing shit

--- test.php
+++ test.php
@@ -1,7 +1,26 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/functions.php'); 
-echo "something";
 
-echo '<pre>'; 
-var_dump($tasksData);
-echo '</pre>'; 
+foreach ($tasksData as $task) {
+  echo $task['reward']."<br>";
+}
+
+function sortByReward($x, $y){
+  if ($x['reward'] == $y['reward']){
+    return 0; 
+  } // end if 
+  return ($x['reward'] < $y['reward']) ? 1:-1;
+} // end function 
+
+usort($tasksData, 'sortByReward');
+
+$i = 0; 
+while ($i <= 3){
+  echo $tasksData[$i]['reward']; 
+  $i++."<br>"; 
+} // end while 
+
+
+// echo '<pre>'; 
+// var_dump($tasksData);
+// echo '</pre>'; 
 ?>
